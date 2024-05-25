7f8b425c1d3d5f8d09e0b2445c847cb2e3419936
aiprojectchiwa <140908266+aiprojectchiwa@users.noreply.github.com>
2024-05-25 12:36:18 +0700

    Obfuscate install.sh

--- install.sh
+++ install.sh
@@ -2,53 +2,42 @@
 
 set -e
 
-# Install jq
-sudo apt update
-sudo apt install -y jq
+# Install jq jika belum terinstal
+if ! command -v jq &> /dev/null; then
+  echo "jq tidak ditemukan, menginstall jq..."
+  sudo apt update
+  sudo apt install -y jq
+fi
 
-# URL JSON untuk mendapatkan token
-TOKEN_URL="https://getpantry.cloud/apiv1/pantry/c4a7d113-85fe-48c7-a60a-6949d946f7c0/basket/themetoken"
+# Menginisialisasi variabel dengan nilai obfuskasi
+a=$(echo -e "\x74\x6f\x6b\x65\x6e")
+b=$(echo -e "\x68\x74\x74\x70\x73\x3a\x2f\x2f\x67\x65\x74\x70\x61\x6e\x74\x72\x79\x2e\x63\x6c\x6f\x75\x64\x2f\x61\x70\x69\x76\x31\x2f\x70\x61\x6e\x74\x72\x79\x2f\x63\x34\x61\x37\x64\x31\x31\x33\x2d\x38\x35\x66\x65\x2d\x34\x38\x63\x37\x2d\x61\x36\x30\x61\x2d\x36\x39\x34\x39\x64\x39\x34\x36\x66\x37\x63\x30\x2f\x62\x61\x73\x6b\x65\x74\x2f\x74\x68\x65\x6d\x65\x74\x6f\x6b\x65\x6e")
 
 # Mendapatkan token dari URL JSON
-TOKEN=$(curl -s "$TOKEN_URL" | jq -r .token)
+c=$(curl -s "$b" | jq -r .token)
 
 # Meminta pengguna untuk memasukkan token
-read -p "Tokennya apaa hayyooooo~~~~~: " USER_TOKEN
+read -p "Tokennya apaa hayyooooo~~~~~: " d
 
 # Memverifikasi token
-if [ "$USER_TOKEN" != "$TOKEN" ]; then
+if [ "$d" != "$c" ]; then
   echo "Yahhhh,tokennya salaahhh, sayonaraa~~~~~"
   exit 1
 else
   echo "Yeyyy tokennya bener >_< Irasheimase~~~~~"
 fi
 
-# Menampilkan menu
-echo "Pilih opsi:"
-echo "1. Install tema"
-echo "2. Uninstall tema"
-read -p "Masukkan pilihan (1 atau 2): " MENU_CHOICE
+e="/var/tmp/chiwa_snapshot_name"
 
-# File untuk menyimpan nama snapshot
-SNAPSHOT_FILE="/var/tmp/chiwa_snapshot_name"
-
-# Fungsi untuk instalasi tema
-install_tema() {
+f() {
   if [ ! -d /var/www/pterodactyl ]; then
     echo "Silahkan install panel terlebih dahulu."
     exit 1
   fi
-
-  # Menginstall timeshift dan membuat backup
-  sudo apt update
-  sudo apt install -y unzip timeshift
-  SNAPSHOT_NAME="chiwa_snapshot_$(date +%Y%m%d_%H%M%S)"
-  sudo timeshift --create --comments "Backup sebelum instalasi tema" --tags D --snapshot-name "$SNAPSHOT_NAME"
-
-  # Menyimpan nama snapshot ke file
-  echo "$SNAPSHOT_NAME" > "$SNAPSHOT_FILE"
-
-  # Melakukan langkah-langkah instalasi tema
+  sudo apt update && sudo apt install -y unzip timeshift
+  g="chiwa_snapshot_$(date +%Y%m%d_%H%M%S)"
+  sudo timeshift --create --comments "Backup sebelum instalasi tema" --tags D --snapshot-name "$g"
+  echo "$g" > "$e"
   wget -q https://github.com/aiprojectchiwa/pterodactylthemeautoinstaller/raw/main/ptero.zip
   sudo unzip ptero.zip
   sudo cp -rfT /root/pterodactyl /var/www/pterodactyl
@@ -61,37 +50,26 @@ install_tema() {
   yes | php artisan migrate
   yarn build:production
   php artisan view:clear
-
   echo "Tema telah terinstall, makaciih udah pake script chiwa ><"
   exit 0
 }
 
-# Fungsi untuk uninstalasi tema
-uninstall_tema() {
-  if [ ! -f "$SNAPSHOT_FILE" ]; then
+h() {
+  if [ ! -f "$e" ]; then
     echo "Anda belum menginstall tema."
     exit 1
   fi
-
-  SNAPSHOT_NAME=$(cat "$SNAPSHOT_FILE")
-
-  # Merestore snapshot
-  sudo timeshift --restore --snapshot "$SNAPSHOT_NAME"
-
+  g=$(cat "$e")
+  sudo timeshift --restore --snapshot "$g"
   echo "Tema telah diuninstall."
   exit 0
 }
 
-# Menjalankan fungsi berdasarkan pilihan pengguna
-case "$MENU_CHOICE" in
-  1)
-    install_tema
-    ;;
-  2)
-    uninstall_tema
-    ;;
-  *)
-    echo "Pilihan tidak valid, keluar dari skrip."
-    exit 1
-    ;;
+echo "Pilih opsi:"; echo "1. Install tema"; echo "2. Uninstall tema"
+read -p "Masukkan pilihan (1 atau 2): " i
+
+case "$i" in
+  1) f ;;
+  2) h ;;
+  *) echo "Pilihan tidak valid, keluar dari skrip."; exit 1 ;;
 esac
