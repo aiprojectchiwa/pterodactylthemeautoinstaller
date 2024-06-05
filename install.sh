e593f43f8b09ebeaf94af23983e7ea256f57406e
aiprojectchiwa <140908266+aiprojectchiwa@users.noreply.github.com>
2024-06-05 16:29:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
-######################################################################################
-#                                                                                    #
-# Project 'pterodactyl-theme-installer'                                              #
-#                                                                                    #
-# Copyright (C) 2023 - 2024, @akane_chiwa,    akane_chiwa@gmail.com                  #
-#                                                                                    #
-#   This program is free software: you can redistribute it and/or modify             #
-#   it under the terms of the GNU General Public License as published by             #
-#   the Free Software Foundation, either version 3 of the License, or                #
-#   (at your option) any later version.                                              #
-#                                                                                    #
-#   This program is distributed in the hope that it will be useful,                  #
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of                   #
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    #
-#   GNU General Public License for more details.                                     #
-#                                                                                    #
-#   You should have received a copy of the GNU General Public License                #
-#   along with this program.  If not, see <https://www.gnu.org/licenses/>.           #
-#                                                                                    #
-# This script is not associated with the official Pterodactyl Project.               #
-# https://github.com/aiprojectchiwa/pterodactylthemeautoinstaller                    #
-#                                                                                    #
-######################################################################################
-
+#########################################################################
+# Project 'pterodactyl-theme-installer' | © 2023-2024, @akane_chiwa,    #
+# akane_chiwa@gmail.com                                                 #
+#                                                                       #
+# This program is free software: you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+# GNU General Public License for more details.                          #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with this program. If not, see <https://www.gnu.org/licenses/>. #
+#                                                                       #
+# This script is not associated with the official Pterodactyl Project.  #
+# https://github.com/aiprojectchiwa/pterodactylthemeautoinstaller       #
+#########################################################################
 set -e
 
 # Warna
